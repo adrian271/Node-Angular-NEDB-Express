a2a71ca926bd8aab72e57928d80db49926f0d5ef
Adrian Barnes <abarnes@billabong.com>
2017-02-24 14:43:02 -0800

    extend paths

--- app.js
+++ app.js
@@ -17,26 +17,27 @@ var app = express();
 app.set('views', path.join(__dirname, 'views')); // '__dirname' gets the current directory where your file resides... path.join helps concatenate so you don't end up with different OS conflicts on your node install
 app.set('view engine', 'ejs'); // Note that we didn't need to decalre EJS in the above variables, Express already works this out for EJS
 
-app.use(logger("The Redirector"));
+app.use(logger("Requested: "));
 app.use(express.static(path.join(__dirname, 'public'))); //This automatically comes from express and serves as a static file server
 
 app.get('/', function(req,res){
 	mappings.list(function (err, documents){
-		res.render('index');	
+		res.render('index');
 	})
 });
 
-app.get('/:alias', function(req, res){
-	mappings.get(req.params.alias, function(err, mapping){
-		if (err) {return res.status(404).end();}
-		res.redirect(mapping);	
-	});	
+app.get('*', function(req, res){
+	// mappings.get(req.params.alias, function(err, mapping){
+	// 	if (err) {return res.status(404).end();}
+	// 	res.redirect(mapping);
+	// });
+	res.send('');
 });
 
 
 //mappings.create("v","http://www.vimeo.com",function(alias, url){console.log(alias+" "+url)}); //Edit this line to add more
 
-var port = 3000;
+var port = 3005;
 
 
 var server = http.createServer(app);
